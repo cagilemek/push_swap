4da8aae6cfb20536843acc2a625ad5af75663e65
Muhammed Omer Celep <mucelep@k1m26s09.42istanbul.com.tr>
2026-04-01 19:35:38 +0300

    addfront deleted

--- helper.c
+++ helper.c
@@ -6,7 +6,7 @@
 /*   By: mucelep <mucelep@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 01:54:20 by mucelep           #+#    #+#             */
-/*   Updated: 2026/03/31 04:38:44 by mucelep          ###   ########.fr       */
+/*   Updated: 2026/04/01 19:35:22 by mucelep          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	while (tmp->next)
 		tmp = tmp->next;
 	tmp->next = new;
-}
-
-void	ft_lstadd_front(t_list **lst, t_list *new)
-{
-	if (!lst || !new)
-		return ;
-	new->next = *lst;
-	*lst = new;
 }
