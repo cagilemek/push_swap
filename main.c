19eaf757c14127b8d8a4fa913fd5ec124bc4ca8e
Muhammed Omer Celep <mucelep@k1m26s09.42istanbul.com.tr>
2026-04-02 02:40:04 +0300

    main

--- main.c
+++ main.c
@@ -3,46 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ckurtul <ckurtul@student.42istanbul.com    +#+  +:+       +#+        */
+/*   By: mucelep <mucelep@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 02:17:48 by ckurtul           #+#    #+#             */
-/*   Updated: 2026/04/02 02:17:59 by ckurtul          ###   ########.fr       */
+/*   Updated: 2026/04/02 02:32:17 by mucelep          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+int main(int argc, char **argv)
 {
-	t_stack *stacka;
-	t_list *a;
-	stacka = malloc(sizeof(t_stack));
-	a = malloc(sizeof(t_list));
-	a->value = 1;
-	
-	t_list *b;
-	b = malloc(sizeof(t_list));
-	b->value = 2;
-	a->next = b;
-	b->next = NULL;
-	t_list *tmp;
-	tmp = a;
-	while (tmp)
-	{
-		printf("%d\n",tmp->value);
-		tmp = tmp->next;
-	}
-
-	stacka->a = a;
-	sa(stacka);
-	tmp = stacka->a;
-	while (tmp)
-	{
-		printf("%d\n",tmp->value);
-		
-		tmp = tmp->next;
-	}
-	printf("\n%d\n",stacka->count.sa);
-	
+	t_stack *stacks;
 }
