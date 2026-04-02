23cc61baef3d1f376a530a23ece32a76ebbf3250
Cagil Emek Kurtul <ckurtul@k1m26s07.42istanbul.com.tr>
2026-04-02 20:06:56 +0300

    aciklama eklendi

--- disorder.c
+++ disorder.c
@@ -6,7 +6,7 @@
 /*   By: ckurtul <ckurtul@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 18:19:00 by ckurtul           #+#    #+#             */
-/*   Updated: 2026/04/02 19:41:02 by ckurtul          ###   ########.fr       */
+/*   Updated: 2026/04/02 20:06:30 by ckurtul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ float disorder (int *atol, int size)
 	
 	mistakes = 0;
 	total_pairs = 0;
-	i = 0;
+	i = 0; // i ve j de sırayla sayıları tutuyor liste sıralı mı diye kontrol etmek için
 	j = 0;
 	
 	if(size <= 1)
@@ -32,13 +32,13 @@ float disorder (int *atol, int size)
 		while (j < size)
 		{
 			total_pairs += 1;
-			if(atol[i] > atol[j])
+			if(atol[i] > atol[j]) // atol tek tek kontrol ettiği için kullanıyoruz split kullanmadık çünkü split char onu castlemekle uğraşmamız gerekirdi
 				mistakes += 1;
 			j++;
 		}
 		i++;
 	}
-	return(mistakes/total_pairs);
+	return(mistakes/total_pairs); //burda oranı buluyoruz
 }
 
 void strategy_selector (int *arr, int size)
@@ -47,7 +47,7 @@ void strategy_selector (int *arr, int size)
 	
 	d = disorder(arr, size);
 
-	if (d == 0)
+	if (d == 0) // just seçim
 		return;
 	else if (d < 0.2)
 		simple_sort(arr, size);
