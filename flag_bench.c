929c9aa886a26964734d1b146d770193fda0cdb7
Muhammed Omer Celep <mucelep.celepm82@gmail.com>
2026-04-20 23:39:05 +0300

    bench flag

--- flag_bench.c
+++ flag_bench.c
@@ -6,54 +6,32 @@
 /*   By: mucelep <mucelep@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 15:35:33 by mucelep           #+#    #+#             */
-/*   Updated: 2026/04/15 17:22:42 by mucelep          ###   ########.fr       */
+/*   Updated: 2026/04/20 23:33:47 by mucelep          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	bench_press(t_stack *s)
+void	bench_press(t_stack *s, float f)
 {
 	if (!s->bench)
 		return ;
+	int		integer;
+	float	flo;
+	int		decimal;
 
-	print_line("total: ", s->count.total);
-	print_line("sa: ", s->count.sa);
-	print_line("sb: ", s->count.sb);
-	print_line("ss: ", s->count.ss);
-	print_line("pa: ", s->count.pa);
-	print_line("pb: ", s->count.pb);
-	print_line("ra: ", s->count.ra);
-	print_line("rb: ", s->count.rb);
-	print_line("rr: ", s->count.rr);
-	print_line("rra: ", s->count.rra);
-	print_line("rrb: ", s->count.rrb);
-	print_line("rrr: ", s->count.rrr);
+	disorder_to_integer(&integer, &flo, &decimal, f);
+	
+	ft_printf("[bench] disorder: %d.%d%%\n",integer, decimal);
+	ft_printf("[bench] total_ops: %d\n",s->count.total);
+	ft_printf("[bench] str:");
+	
+
 }
 
-void	print_line(char *name, int value)
+void disorder_to_integer(int *integer, float *flo, int *decimal, float f)
 {
-	write(2, name, ft_strlen(name));
-	ft_putnbr_fd(value, 2);
-	write(2, "\n", 1);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-void	ft_putnbr_fd(int value, int fd)
-{
-	char	c;
-
-	if (value >= 10)
-		ft_putnbr_fd(value / 10, fd);
-	c = (value % 10) + '0';
-	write(fd, &c, 1);
+	*integer = 100 * f;//float sayının ilk 2 kısmı 49.12 (49)
+	*flo = 100 * f;//float sayının diger 2 kısmı 49.12 (12)
+	*decimal = (*flo - *integer) * 100;
 }
